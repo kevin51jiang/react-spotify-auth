5b3d6fe05da498d303d96c93e8da092fad1bf72f
Kevin Jiang <kevin51jiang@email.com>
2020-06-24 01:11:26 -0400

    Updates

---
+++ precache-manifest.16be2fff5b6ccfbbca1c3de81af663b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca3f133759dd11c518ea159e1c3c5b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a32a8c668e6bf8cbe60",
+    "url": "./static/css/2.5907f6f0.chunk.css"
+  },
+  {
+    "revision": "2001e76915e3a039b4be",
+    "url": "./static/css/main.464090d0.chunk.css"
+  },
+  {
+    "revision": "4a32a8c668e6bf8cbe60",
+    "url": "./static/js/2.11f21406.chunk.js"
+  },
+  {
+    "revision": "870e3c5359799be6ca7ed5db7d034ed4",
+    "url": "./static/js/2.11f21406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2001e76915e3a039b4be",
+    "url": "./static/js/main.9901b068.chunk.js"
+  },
+  {
+    "revision": "96c0d23e308070baa19b",
+    "url": "./static/js/runtime-main.052efaa7.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
