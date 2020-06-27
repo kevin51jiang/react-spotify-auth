d85f93ad988655e959e283e8092acd1ec767da4a
Kevin Jiang <kevin51jiang@email.com>
2020-06-26 23:41:32 -0400

    Button finished being styled nicely

--- src/SpotifyAuth.jsx
+++ src/SpotifyAuth.jsx
@@ -82,7 +82,7 @@ SpotifyAuth.defaultProps = {
   onAccessToken: () => console.log('I have an access token'),
   containerClassName: 'spotify-signin-container',
   btnClassName: 'spotify-signin-btn',
-  title: 'Continue with Spotify'
+  title: 'CONTINUE WITH SPOTIFY'
 }
 
 export default SpotifyAuth
